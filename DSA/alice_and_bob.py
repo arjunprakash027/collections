bc1e2caa02dca03ac308ccf7092709f07ffe2272
arjun <arjunprakash027@gmail.com>
2023-03-21 00:14:47 +0530

    dsa day1

---
+++ DSA/alice_and_bob.py
@@ -0,0 +1,27 @@
+def locate_card(cards,query):
+    position = 0
+
+    while True:
+        if cards[position] == query:
+            return position
+        
+        position += 1
+
+        if position == len(cards):
+            return -1
+
+cards = [13,11,10,7,4,3,1]
+query = 7
+output = 3
+
+result = locate_card(cards,query)
+
+test = {
+    "input": {
+        "cards": [13,11,10,7,4,3,1],
+        "query": 7
+    },
+    "output": 3
+}
+
+print(locate_card(**test["input"]) == test["output"])
