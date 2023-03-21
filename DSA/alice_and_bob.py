1babfecaaef4612c0d1a69acd29372587b59002a
arjun <arjunprakash027@gmail.com>
2023-03-22 00:27:20 +0530

    day2

--- DSA/alice_and_bob.py
+++ DSA/alice_and_bob.py
@@ -25,3 +25,22 @@ test = {
 }
 
 print(locate_card(**test["input"]) == test["output"])
+
+
+def locate_card_binary(cards,query):
+    lo,hi = 0, len(cards)-1
+
+    while lo <= hi:
+        mid = (lo+hi) // 2
+        mid_number = cards[mid]
+
+        if mid_number == query:
+            return mid
+        elif mid_number < query:
+            hi = mid - 1
+        elif mid_number > query:
+            lo = mid + 1
+
+    return -1 
+
+print(locate_card_binary(**test["input"]) == test["output"])
