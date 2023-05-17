175e08f073cbdcc6b3969b48d8f3bf34b4b66960
Arjun P <arjunprakash027@gmail.com>
2023-05-17 21:46:17 +0530

    scrap multiple pages

---
+++ tutorials/scrapy_tutorials/bookscrapper/bookscrapper/spiders/spider1.py
@@ -0,0 +1,68 @@
+import scrapy
+
+
+class Spider1Spider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"] #when crawling multiple domains, it lists domains only we want to scrap
+    start_urls = ["http://books.toscrape.com/"]
+
+    def parse(self, response): # diffent peice of information we need to get extracted
+        books = response.css('article.product_pod')
+
+        for book in books:
+            yield {
+                'name' : book.css('h3 a').attrib['title'],
+                'price' : book.css('div.product_price p.price_color::text').get(),
+                'link' : book.css('h3 a').attrib['href']
+            }
+        
+        next_page = response.css('li.next a::attr(href)').get() #get the next button link in the end of the page to navigate to the next page
+
+        if next_page is not None: #recursively use the parse until there is no next button
+            if 'catalogue/' in next_page:
+                next_page_url = self.start_urls[0] + next_page
+            else:
+                next_page_url = self.start_urls[0] + "catalogue/" + next_page
+            yield response.follow(next_page_url, callback = self.parse)
+
+
+class Spider2Spider(scrapy.Spider):
+    name = "indibook"
+    allowed_domains = ["books.toscrape.com"] #when crawling multiple domains, it lists domains only we want to scrap
+    start_urls = ["http://books.toscrape.com/"]
+
+    def parse(self, response): # diffent peice of information we need to get extracted
+        books = response.css('article.product_pod')
+
+        for book in books:
+            book_details = book.css('h3 a ::attr(href)').get()
+
+            if 'catalogue/' in book_details:
+                book_details_url = self.start_urls[0] + book_details
+            else:
+                book_details_url = self.start_urls[0] + "catalogue/" + book_details
+
+            yield response.follow(book_details_url, callback=self.parse_book_page)
+        
+        next_page = response.css('li.next a::attr(href)').get() #get the next button link in the end of the page to navigate to the next page
+
+        if next_page is not None: #recursively use the parse until there is no next button
+            if 'catalogue/' in next_page:
+                next_page_url = 'http://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'http://books.toscrape.com/' + "catalogue/" + next_page
+            yield response.follow(next_page_url, callback = self.parse)
+    
+    def parse_book_page(self,response):
+        table_rows = response.css("table tr")
+
+        yield {
+            'url' : response.url,
+            'title' : response.css('div.product_main h1::text').get(),
+            'price' : response.css('p.price_color ::text').get(),
+            'type' : table_rows[1].css("td ::text").get(),
+            'aviliblity' : table_rows[5].css("td ::text").get(),
+            'rating' : response.css('p.star-rating').attrib['class'][11:],
+            'category' : response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+            'description' : response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+        }
