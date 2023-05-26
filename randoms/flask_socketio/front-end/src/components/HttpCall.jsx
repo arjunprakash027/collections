05c4061057615170304a91890ae4ddf1ac900f8e
Arjun P <arjunprakash027@gmail.com>
2023-05-26 17:50:08 +0530

    react application

---
+++ randoms/flask_socketio/front-end/src/components/HttpCall.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function HttpCall() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("/http-call", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData["status"]);
+        console.log(responseData["status"]);
+      });
+  });
+  return (
+    <>
+      <h2>HTTP Communication</h2>
+      <h3 className="http">{data}</h3>
+    </>
+  );
+}
