3791fac1ae6f6a62d21fc63ace776a11e86029ac
Arjun P <arjunprakash027@gmail.com>
2023-06-11 20:04:22 +0530

    ccc

---
+++ DSA/CCC Collage training/turtle_ccc.py
@@ -0,0 +1,21 @@
+
+import turtle
+
+wn = turtle.Screen()
+wn.setup(500,500)
+
+alex = turtle.Turtle()
+alex.shape("turtle")
+alex.color("red")
+
+while True:
+    alex.circle(50)
+    alex.forward(100)
+    alex.left(90)
+    alex.forward(100)
+    alex.left(90)
+    alex.forward(200)
+    alex.left(90)
+    alex.forward(100)
+    alex.left(90)
+    alex.forward(100)
